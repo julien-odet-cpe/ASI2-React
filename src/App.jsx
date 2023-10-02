bb6e332a9438aa53046d8b7d16e0292419ace6de
Julien ODET <julien.odet@cpe.fr>
2023-10-02 10:49:57 +0200

    Fin STEP 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { Robot } from './components/Robot';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [robots, setRobots] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const resp = await fetch(
+        'https://www.robots.loiclegoff.com/robots'
+      );
+
+      const result = await resp.json();
+      setRobots(result);
+    }
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Robot Factory</h1>
+      {robots.map((robot) => (
+        <Robot
+          key={robot.id}
+          id={robot.id}
+          title={robot.title}
+          visual_src={robot.visual_src}
+        />
+      ))}
+      <p>{JSON.stringify(robots)}</p>
+    </div>
+  );
 }
-
-export default App
+export default App;
